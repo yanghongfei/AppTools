bc797a8d9f15fa379c9468f7930f613a88476b13
杨红飞 <yanghongfei@shinezone.com>
2018-11-20 15:47:03 +0800

    更新依赖

---
+++ requirements/README.md
@@ -0,0 +1,11 @@
+## 安装依赖说明
+
+- 依赖包安装
+```
+pip3 install -r requirements.txt
+```
+
+- 阿里云SMS 包不支持pip3 安装，需要另外使用pip
+```
+pip install  -r requirements_sms.txt
+```
---
+++ requirements/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/20 15:42
+# @Author  : Fred Yang
+# @File    : __init__.py.py
+# @Role    : 说明脚本功能
